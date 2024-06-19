d424d783bc40bef5b4d4bef1d0e83a015328cfdd
khsik <maegoxi1@gmail.com>
2024-06-19 17:31:22 +0900

    write using summernote

--- springboot/planner/src/main/java/com/planner/controller/TeamBoardController.java
+++ springboot/planner/src/main/java/com/planner/controller/TeamBoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -67,7 +68,7 @@ public class TeamBoardController {
 	}
 	
 	@GetMapping("/write")
-	public String tbwrite(@RequestParam("team_id")Long team_id, @RequestParam("team_member_id")Long team_member_id) {
+	public String tbwrite(@ModelAttribute("team_id")Long team_id, @ModelAttribute("team_member_id")Long team_member_id) {
 		
 		return "/team/board/tbwrite";
 	}
